9fb1683870b84ca69f99662afbd58b6603ea8929
Alina5002 <amor120121@gmail.com>
2026-02-25 16:47:06 +0300

    добавлены комментарии

--- src/tag-service.js
+++ src/tag-service.js
@@ -1,12 +1,32 @@
+import { Tag } from "../models/tag.js";
+
+/**
+ * Сервис для управления ярлыками.
+ * Отвечает за создание, хранение и получение списка ярлыков.
+ */
 export class TagService {
+  /**
+   * Инициализирует сервис ярлыков.
+   */
   constructor() {
+    /** @type {Tag[]} */
     this.tags = [];
   }
 
+  /**
+   * Добавляет новый ярлык в систему.
+   *
+   * @param {Tag} tag - Экземпляр ярлыка.
+   */
   addTag(tag) {
     this.tags.push(tag);
   }
 
+  /**
+   * Возвращает список всех ярлыков.
+   *
+   * @returns {Tag[]} Массив ярлыков.
+   */
   getAll() {
     return this.tags;
   }
--- src/tag.js
+++ src/tag.js
@@ -1,6 +1,20 @@
+/**
+ * Класс Tag представляет ярлык,
+ * который может быть назначен заметке
+ * для категоризации и быстрого поиска.
+ */
 export class Tag {
+  /**
+   * Создает новый экземпляр ярлыка.
+   *
+   * @param {string|number} id - Уникальный идентификатор ярлыка.
+   * @param {string} name - Название ярлыка.
+   */
   constructor(id, name) {
+    /** @type {string|number} */
     this.id = id;
+
+    /** @type {string} */
     this.name = name;
   }
 }
